16cca59e8e6c2b1730ee2d1bb6fe49d1fc3af0c0
Olga.Reznik <olga.reznik@ing.nl>
2017-04-18 18:46:35 +0200

    update taxis job

--- src/main/scala/io/github/rezolya/intro/flink/exercises/TaxisJob.scala
+++ src/main/scala/io/github/rezolya/intro/flink/exercises/TaxisJob.scala
@@ -1,10 +1,15 @@
 package io.github.rezolya.intro.flink.exercises
 import com.dataartisans.flinktraining.exercises.datastream_java.datatypes.TaxiRide
 import com.dataartisans.flinktraining.exercises.datastream_java.sources.TaxiRideSource
+import com.dataartisans.flinktraining.exercises.datastream_java.utils.GeoUtils
 import org.apache.flink.api.scala._
 import org.apache.flink.core.fs.FileSystem.WriteMode
 import org.apache.flink.streaming.api.TimeCharacteristic
-import org.apache.flink.streaming.api.scala.{DataStream, StreamExecutionEnvironment}
+import org.apache.flink.streaming.api.functions.sink.SocketClientSink
+import org.apache.flink.streaming.api.scala.{DataStream, KeyedStream, StreamExecutionEnvironment}
+import org.apache.flink.streaming.api.windowing.assigners.{SlidingEventTimeWindows, SlidingProcessingTimeWindows}
+import org.apache.flink.streaming.api.windowing.time.Time
+import org.apache.flink.streaming.util.serialization.SerializationSchema
 
 /**
   * DataStream Job to play with TaxiRide data stream, provided by data artisans for training purposes
@@ -20,40 +25,46 @@ object TaxisJob {
     //env.setParallelism(1)
 
     val maxDelay = 1
-    val servingSpeed = 60
+    val servingSpeed = 10
 
     // get the taxi ride data stream
     val rides: DataStream[TaxiRide] = env.addSource(
       new TaxiRideSource("/tmp/nycTaxiRides.gz", maxDelay, servingSpeed))
 
-    //TODO: Exercise 2.1. Write the taxi rides to a socket stream, and see how this works
-
+    /*
     //TODO: Exercise 2.2. Taxi Ride Cleansing. Filter out only taxi rides which start and end in the New York City
     //GeoUtils.isInNYC() can tell you whether a location is in NYC.
     val nyRides: DataStream[TaxiRide] = ???
-    nyRides.writeAsText("/tmp/taxis/nyRides.txt", WriteMode.OVERWRITE)
 
-    /* TODO: Exercise 3. Popular places. Find popular places by counting places where the taxi rides stop or start
-       use GeoUtils.mapToGridCell(float lon, float lat) to group locations
-       count every five minutes the number of taxi rides that started and ended in the same area within the last 15 minutes
-    */
+    /*
+    //TODO: Exercise 3. Popular places. Find popular places by counting places where the taxi rides stop or start
+    //use GeoUtils.mapToGridCell(float lon, float lat) to group locations
+    //count every five minutes the number of taxi rides that started and ended in the same area within the last 15 minutes
 
     //1. determine the sells of start and end of the taxi rides
     // result type: (Int - cell id, TaxiRide)
     val withCell: DataStream[(Int, TaxiRide)] = ???
 
+    /*
     //2. find out how many times each cell is visited
     // result type: (Int - cell id, Long - timestamp of the count, Boolean - arrival or departure, Int - count)
-    val cellVisits: DataStream[(Int, Long, Boolean, Int)] = ???
+    val cellVisits: DataStream[(Int, Long, Int)] = ???
 
+    /*
     //3. filter the popular cells only - the ones that have more visits that popularityThreshold
     val popularityThreshold = 20
-    val popularCells: DataStream[(Int, Long, Boolean, Int)] = ???
+    val popularCells: DataStream[(Int, Long, Int)] = ???
 
     //4. convert the cell id to coordinates of the cell (use GeoUtils.getGridCellCenterLat(cellId))
     // result type: (Float - cell longitude, Float - cell latitude, Long - timestamp of the count, Boolean - arrival or departure, Int - count)
-    val popularPlaces: DataStream[(Float, Float, Long, Boolean, Integer)] = ???
-    popularPlaces.writeAsText("/tmp/taxis/popularPlaces.txt", WriteMode.OVERWRITE)
+    val popularPlaces: DataStream[(Float, Float, Long, Integer)] = ???
+    */
+    */
+    */
+    */
+
+    //TODO: Exercise 2.1. Write the taxi rides to a socket stream, and see how this works
+    val stringRides = ???
 
     env.execute()
   }
